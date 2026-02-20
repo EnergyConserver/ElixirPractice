7087f02e2bc3d6d640abbaca03e92a8bb7075a5b
Eduardo Rodríguez Velázquez <a01739279@tec.mx>
2026-02-19 20:11:30 -0600

    version3

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,4 +1,5 @@
 defmodule Bowling do
   def score([]), do: 0
+  def score([[f, s|_]|others]) when f + s == 10, do: f + s + hd(hd(others)) + score(others)
   def score([[f, s|_]|others]), do: f + s + score(others)
 end
