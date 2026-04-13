2a503301db6e580cb170fd214a6e1ce82f51daef
Eduardo Rodríguez Velázquez <a01739279@tec.mx>
2026-04-13 07:19:54 -0600

    Automata

--- automata/lib/automata.ex
+++ automata/lib/automata.ex
@@ -1,18 +1,10 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Automata.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+    def determize({q, _sigma, _delta, _q0, _f}) do
+        q_prime =
+            Enum.reduce(q, [[]], fn x, acc -> acc++ Enum.map(acc, fn subset -> [x | subset] end) end)
+            |> Enum.reject(&(&1 == []))
+            |> Enum.map(&Enum.sort/1)
+            |> Enum.uniq()
+        {q_prime}
+    end
 end
--- automata/test/automata_test.exs
+++ automata/test/automata_test.exs
@@ -2,7 +2,42 @@ defmodule AutomataTest do
   use ExUnit.Case
   doctest Automata
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  test "Determinacion of non-deterministic automata" do
+    N = {
+      [Q0, Q1, Q2, Q3],
+      #[A, B],
+      #%{
+        #{Q0, A} => [Q0, Q1],
+        #{Q0, B} => [Q0],
+        #{Q1, B} => [Q2],
+        #{Q2, B} => [Q3]
+      #},
+      #Q0,
+      #[Q3]
+    }
+    DN = Automata.determize(N)
+    assert DN == {
+      [
+        [Q0], [Q1], [Q2], [Q3], [Q0, Q1],[Q0, Q2], [Q0, Q3], [Q1, Q2], [Q1, Q3],
+        [Q2, Q3], [Q0, Q1, Q2], [Q0, Q1, Q3], [Q0, Q2, Q3], [Q1, Q2, Q3], [Q0, Q1, Q2, Q3]
+      ],
+      nil,
+      nil,
+      nil,
+      nil
+      #[A, B],
+      #%{
+        #{Q0, A} => [Q1],
+        #{Q0, B} => [Q0],
+        #{Q1, A} => [Q1],
+        #{Q1, B} => [Q2],
+        #{Q2, A} => [Q1],
+        #{Q2, B} => [Q3],
+        #{Q3, A} => [Q1],
+        #{Q3, B} => [Q0]
+      #},
+      #Q0,
+      #[Q3]
+    }
   end
 end
