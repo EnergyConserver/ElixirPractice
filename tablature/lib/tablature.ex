ff3211745cf5476529dc2a398d9ec5b1365f76d7
EnergyConserver <lalinskidn@gmail.com>
2026-05-31 23:24:37 -0600

    Tablature V2

--- tablature/lib/tablature.ex
+++ tablature/lib/tablature.ex
@@ -1,8 +1,14 @@
 defmodule Tablature do
   def parse(tab) do
-    Regex.scan(~r/\d+/, tab)
-    |> List.flatten
-    |> Enum.map(fn n -> "B" <> n end)
-    |> Enum.join(" ")
+    tab
+    |> String.split("\n", trim: true)
+    |> Enum.flat_map(fn line ->
+      [s, notes] = String.split(line, "|", parts: 2)
+
+      Regex.scan(~r/\d+/, notes, return: :index)
+      |> Enum.map(fn [{i, l}] -> {i, s <> String.slice(notes, i, l)} end)
+    end)
+    |> Enum.sort_by(&elem(&1, 0))
+    |> Enum.map_join(" ", &elem(&1, 1))
   end
 end
